39d85c8f9668ac8ea21818f1bdafe412d9fc8867
Wyb0 <andrewwybo@gmail.com>
2017-09-24 11:39:35 -0400

    added random.txt and liri.js files, added twitter keys

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: '	wAUS4ZXjg0Hd6mEkAvRUtVKDa',
+  consumer_secret: 'lL8W4flfP8JfrMZXsbKFjQZK79hkiJIwOw253mo85EXbWkza99',
+  access_token_key: '911976610269679617-NuCY2SJE0S0OZi7C5EWuZPhypdoJ68e',
+  access_token_secret: 'ewohY9FQgjUrcaIR17x02iGvsRyr1B61466BDfIA7FpbZ',
 }
 
 module.exports = twitterKeys;
