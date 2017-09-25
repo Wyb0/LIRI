489b275f84bc2bd2bb1e9260d1ec7e6d346d9627
Andrew Wybo <andrewwybo@gmail.com>
2017-09-25 14:30:19 -0400

    added spotifykeys.js, added spotify keys, updated NPM packages, and added var requires

---
+++ liri.js
@@ -0,0 +1,25 @@
+//require all of packages and files
+var request = require("request")
+var twitterKeys = require("./keys.js")
+console.log(twitterKeys)
+var spotifyKeys = require("./spotifykeys.js")
+var twitter = require("twitter")
+var spotify = require("spotify-web-api-node")
+
+//Twitter, Spotify, Request
+
+//get user input 
+
+//conditional for which command 
+
+//my-tweets
+
+//spotify-this-song
+
+//movie-this
+
+//do-what-it-says
+
+//need functions to call for each case
+
+//make case statement for each command
---
+++ spotifykeys.js
@@ -0,0 +1,6 @@
+var spotifyKeys = {
+    clientId: "799d1ab3d15d47fa95a91105a345fc13",
+    clientSecret: "c8e1706fccc945dab7388dfaba21f422"
+  }
+
+module.exports = spotifyKeys;
